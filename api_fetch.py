c7ece6febe623865949a048781d373fbea7788a3
abrack-gh <a.brack@live.co.uk>
2024-02-09 12:43:27 +0000

    secrets

--- api_fetch.py
+++ api_fetch.py
@@ -2,7 +2,7 @@ import requests
 
 city = 'london'
 country = 'england'
-api_key = '5XEpw81RMTnnTc3IogjVWg==VB0n51BA4N7GkDVM'
+api_key = "api_key"
 api_url = 'https://api.api-ninjas.com/v1/geocoding?city={}&country={}'.format(city, country)
 response = requests.get(api_url + city, headers={'X-Api-Key': api_key})
 if response.status_code == requests.codes.ok:
